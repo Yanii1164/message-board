d74208218f05b5b9c161d34c0e51fd7700152d38
yanii1164 <bokusatuangel.syou@gmail.com>
2015-08-23 05:15:16 +0000

    kadai-challenge2 for heroku

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,12 @@
 class Message < ActiveRecord::Base
     
-     # 名前は必須入力かつ20文字以内
+    # 名前は必須入力かつ20文字以内
     validates :name , length: {  maximum: 20 } , presence: true
-    # 内容は必須入力かつ2文字以上30文字以下
-    validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
+    
+    # 年齢は必須入力かつ1文字以上3文字以内かつ整数のみ
+    validates :age , length: {minimum: 1 , maximum: 3 } , numericality: {:only_integer => true} , presence: true
+    
+    # 内容は必須入力かつ1文字以上30文字以下
+    validates :body , length: {minimum: 1 , maximum: 30 } , presence: true
     
 end
